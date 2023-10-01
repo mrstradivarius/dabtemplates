2f80d4cf31b85c70331d33604b2d89a58c4de24b
Mr. Stradivarius <misterstrad@gmail.com>
2023-10-01 13:39:45 +0900

    Create an edit request after saving the page
    Edit the sandbox of the template list module instead of the module
    itself. If the new module sandbox content is different from the module,
    then create an edit request.

--- README.md
+++ README.md
@@ -59,8 +59,9 @@ on the English Wikipedia.
 
 The script iterates through a category of disambiguation templates. For all
 templates that are not in an exclusion list, the script adds the template and
-all its redirects to a Lua table, and then writes that table to the target Lua
-module page.
+all its redirects to a Lua table. It then writes that table to the sandbox of
+the target Lua module page, and if the sandbox and the target Lua module are
+different, it adds an edit request to the talk page.
 
 Usage:
 
@@ -73,15 +74,34 @@ This script provides the following options:
 - `-cat:<category name>`: The category to search for disambiguation templates.
   By default, this is
   [Category:Disambiguation message boxes](https://en.wikipedia.org/wiki/Category:Disambiguation_message_boxes).
-- `-data-page:<data page>`: The Lua module to write the output to. By default,
-  this is
+- `-data-page:<data page>`: The Lua module where the template list is stored.
+  By default, this is
   [Module:Disambiguation/templates](https://en.wikipedia.org/wiki/Module:Disambiguation/templates).
+- `-data-page-sandbox:<data page sandbox>`: The sandbox page for the template
+  list module. By default, this is
+  [Module:Disambiguation/templates/sandbox](https://en.wikipedia.org/wiki/Module:Disambiguation/templates/sandbox).
+- `-data-page-sandbox-summary:<edit summary>`: The edit summary to use when
+  saving the data page sandbox. By default, this is "Bot: update disambiguation
+  template list".
 - `-exclude:<template 1>,<template 2>,...`: A comma-separated list of templates
   (without the "Template:" prefix) to exclude from the Lua table. By default,
   this is `Dmbox`, meaning
   [Template:Dmbox](https://en.wikipedia.org/wiki/Template:Dmbox) is excluded.
-- `-summary:<edit summary>`: The edit summary to use when saving the data page.
-  By default, this is "Bot: update disambiguation template list".
+- `-data-talk-page:<data talk page>`: The talk page of the template list
+  module. By default, this is
+  [Module talk:Disambiguation](https://en.wikipedia.org/wiki/Module_talk:Disambiguation).
+- `-edit-request-template:<edit request template path>`: Path to a template of
+  the text to use when adding edit requests to the data talk page. This
+  defaults to "stradbot/dabtemplates/templates/edit_request.txt". The template
+  accepts the following parameters:
+  - `$current_date`: The current date in long form (e.g. "7 October 2023").
+  - `$data_page`: The title of the data page.
+  - `$data_page_sandbox`: The title of the data page sandbox.
+  - `$template_category`: The title of the category used to find disambiguation
+    templates.
+- `-edit-request-summary:<edit request summary>`: The edit summary to use when
+  saving edit requests to the data talk page.  By default, this is "Bot: create
+  edit request to update disambiguation template list".
 
 In addition, you can use the
 [global Pywikibot options](https://www.mediawiki.org/wiki/Manual:Pywikibot/Global_Options).
--- stradbot/dabtemplates/__main__.py
+++ stradbot/dabtemplates/__main__.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pathlib import Path
+import string
 import sys
 import traceback
 
@@ -12,8 +15,14 @@ def parse_options():
     options = {
         "cat": "Category:Disambiguation message boxes",
         "data-page": "Module:Disambiguation/templates",
+        "data-page-sandbox": "Module:Disambiguation/templates/sandbox",
+        "data-page-sandbox-summary": "Bot: update disambiguation template list",
         "exclude": ["Dmbox"],
-        "summary": "Bot: update disambiguation template list",
+        "data-talk-page": "Module talk:Disambiguation",
+        "edit-request-template": Path(
+            __file__, "..", "templates", "edit_request.txt"
+        ).resolve(),
+        "edit-request-summary": "Bot: create edit request to update disambiguation template list",
     }
     local_args = pywikibot.handle_args()  # global options
     for arg in local_args:
@@ -51,34 +60,74 @@ def format_data_page(aliases):
         {alias: True for alias in aliases},
         table_keys=lua_serializer.TableKeyFormat.FULL,
     )
-    return "return " + lua_table
+    return "return " + lua_table + "\n"
 
 
-def save_page(site, title, content, summary):
-    """Save a page."""
-    page = pywikibot.Page(site, title=title)
-    if page.text == content:
-        logging.info(f"Content of {page} unchanged; skip saving page")
-        return
-    page.text = content
-    page.save(summary)
+def format_edit_request_text(
+    edit_request_template_path, data_page, data_page_sandbox, template_category
+):
+    """Format the edit request text to post on the data module talk page."""
+    edit_request_template = edit_request_template_path.read_text(encoding="utf-8")
+    return (
+        string.Template(edit_request_template)
+        .substitute(
+            current_date=datetime.utcnow().strftime("%d %B %Y").lstrip("0"),
+            data_page=data_page.title(),
+            data_page_sandbox=data_page_sandbox.title(),
+            template_category=template_category.title(),
+        )
+        .strip()
+    )
 
 
 def main():
+    logging.info(f"Start dabtemplates task")
     options = parse_options()
     site = pywikibot.Site()
+    logging.info(f"Options: {options}")
+    logging.info(f"Site: {site}")
+
+    # Construct the new data module content
+    template_category = pywikibot.Category(site, title=options["cat"])
+    logging.info(f"Fetch templates in {template_category} and their redirects")
     template_aliases = fetch_template_aliases(
-        template_generator=pagegenerators.CategorizedPageGenerator(
-            pywikibot.Category(site, title=options["cat"])
-        ),
-        excluded=set(options["exclude"])
+        template_generator=pagegenerators.CategorizedPageGenerator(template_category),
+        excluded=set(options["exclude"]),
     )
-    save_page(
-        site=site,
-        title=options["data-page"],
-        content=format_data_page(template_aliases),
-        summary=options["summary"],
+    data_page_content = format_data_page(template_aliases)
+
+    # Check if the module sandbox content would change, and exit if not
+    data_page_sandbox = pywikibot.Page(site, title=options["data-page-sandbox"])
+    logging.info(f"Check if {data_page_sandbox} content needs updating")
+    if data_page_sandbox.text == data_page_content:
+        logging.info(f"Content of {data_page_sandbox} would not change; skip saving page")
+        return
+
+    # Save the new module content
+    data_page_sandbox.save(options["data-page-sandbox-summary"], text=data_page_content)
+
+    # Check if the module content would change, and exit if not
+    data_page = pywikibot.Page(site, title=options["data-page"])
+    logging.info(f"Check if {data_page} content needs updating")
+    if data_page.text == data_page_content:
+        logging.info(
+            f"Content of {data_page} is identical to new sandbox content; "
+            f"skip adding edit request"
+        )
+        return
+
+    # Create an edit request for the new content
+    data_talk_page = pywikibot.Page(site, title=options["data-talk-page"])
+    logging.info(f"Post edit request at {data_talk_page}")
+    edit_request_text = format_edit_request_text(
+        Path(options["edit-request-template"]),
+        data_page,
+        data_page_sandbox,
+        template_category,
     )
+    data_talk_page.text = f"{data_talk_page.text.rstrip()}\n\n{edit_request_text}\n"
+    data_talk_page.save(options["edit-request-summary"])
+    logging.info(f"End dabtemplates task")
 
 
 if __name__ == "__main__":
